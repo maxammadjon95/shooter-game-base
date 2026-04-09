e982d2e7572658d33f8a5e3a7e50e5de421c4075
Makhammadjon <mr.mahammadjon@mail.ru>
2026-04-09 16:22:14 +0500

    added new scripts

---
+++ Assets/Scripts/CameraFollower.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class CameraFollower : MonoBehaviour
+{
+    [SerializeField] private Transform _target;
+
+
+    private void LateUpdate()
+    {
+
+        transform.position = _target.position;
+
+        transform.rotation = _target.rotation;
+
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,72 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private CharacterController controller;
+    [SerializeField] private Transform cameraPoint;
+    [SerializeField] private float mouseSensitivity = 100f;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundMask;
+    [SerializeField] private float gravity = -9.81f;
+    [SerializeField] private float jumpForce = 5f;
+
+    private float _xRotation = 0f;
+    private float _groundDistance = .25f, yVelocity;
+    private bool isGrounded;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        //JUMP logic
+        isGrounded = Physics.CheckSphere(groundCheck.position, _groundDistance, groundMask);
+
+        if (isGrounded && yVelocity < 0)
+        {
+            yVelocity = -2f;
+        }
+        yVelocity += gravity * Time.deltaTime;
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            Jump();
+        }
+
+        // Get input 
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+
+        // Create movement direction 
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        Vector3 finalMove = move * moveSpeed;
+        finalMove.y = yVelocity;
+
+
+        // Move player 
+        controller.Move(finalMove * Time.deltaTime);
+
+        Debug.Log("MouseSensiticity" + mouseSensitivity);
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        // Vertical rotation (camera) 
+        _xRotation -= mouseY;
+        _xRotation = Mathf.Clamp(_xRotation, -80f, 80f);
+
+        cameraPoint.localRotation = Quaternion.Euler(_xRotation, 0f, 0f);
+
+        // Horizontal rotation (player) 
+        transform.Rotate(Vector3.up * mouseX);
+    }
+
+    private void Jump()
+    {
+        yVelocity = jumpForce;
+    }
+}
