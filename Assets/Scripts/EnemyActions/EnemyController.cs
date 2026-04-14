c3c443dd8d24f110ee1671e889abcfd3eea713b1
Makhammadjon <mr.mahammadjon@mail.ru>
2026-04-14 17:31:03 +0500

    added enemy controller

---
+++ Assets/Scripts/EnemyActions/EnemyController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+
+namespace Assets.Scripts.EnemyActions
+{
+	public class EnemyController : MonoBehaviour
+	{
+		private Tweener _tweener;
+
+        private void Start()
+        {
+            _tweener?.Kill();
+            _tweener = transform.DOMoveX(transform.position.x + 25, 10);
+        }
+    }
+}
